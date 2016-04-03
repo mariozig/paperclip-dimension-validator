0aa31b06d6ba4396925a5d0be4274cc655954512
Mario Zigliotto <mariozig@gmail.com>
2016-04-03 15:35:48 -0700

    Fix broken logic and shrink validation error messages

--- lib/paperclip/validators/attachment_at_least_dimensions_validator.rb
+++ lib/paperclip/validators/attachment_at_least_dimensions_validator.rb
@@ -14,21 +14,13 @@ module Paperclip
 
         begin
           dimensions = Paperclip::Geometry.from_file(value.queued_for_write[:original].path)
-
-          record.errors.add(attribute.to_sym, :dimension, :height, actual_dimension: dimensions.height.to_i, dimension: options[:height]) unless dimensions.height >= options[:height].to_f
-          record.errors.add(attribute.to_sym, :dimension, :width, actual_dimension: dimensions.width.to_i, dimension: options[:width]) unless dimensions.width >= options[:width].to_f
+          record.errors.add(attribute.to_sym, :height_at_least, actual_dimension: dimensions.height.to_i, dimension: options[:dimensions][:height]) unless dimensions.height >= options[:dimensions][:height].to_f
+          record.errors.add(attribute.to_sym, :width_at_least, actual_dimension: dimensions.width.to_i, dimension: options[:dimensions][:width]) unless dimensions.width >= options[:dimensions][:width].to_f
         rescue Paperclip::Errors::NotIdentifiedByImageMagickError
           Paperclip.log("cannot validate dimensions on #{attribute}")
         end
       end
     end
 
-    module HelperMethods
-      def validates_attachment_dimensions(*attr_names)
-        options = _merge_attributes(attr_names)
-        validates_with(AttachmentDimensionsValidator, options.dup)
-        validate_before_processing(AttachmentDimensionsValidator, options.dup)
-      end
-    end
   end
 end
